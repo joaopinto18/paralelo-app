83cc983bf7dfa88a33a32abe2cf971ef991b85ae
Joao Pinto <joaopintotovar2000@gmail.com>
2021-03-10 14:26:30 -0400

    creacion del formulario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaInicioComponent } from './pages/paginaPrincipal/vista-inicio/vista-inicio.component';
+import { VistaDatosPerfilMecanicoComponent } from './pages/perfilMecanico/vista-datos-perfil-mecanico/vista-datos-perfil-mecanico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'vista-inicio', component:VistaInicioComponent},
+  {path:'vista-perfil-mecanico', component:VistaDatosPerfilMecanicoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
