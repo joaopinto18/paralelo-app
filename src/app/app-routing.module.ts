9e284315262a53849ef8f1ec4ac424f9fa0c0a7b
Your NameGabriel <agbalza23@gmail.com>
2021-03-10 19:17:24 -0400

    Registro listo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VistaInicioSesionComponent } from './pages/paginaPrincipal/vista-inicio-sesion/vista-inicio-sesion.component';
 import { VistaInicioComponent } from './pages/paginaPrincipal/vista-inicio/vista-inicio.component';
+import { VistaRegistrarseComponent } from './pages/paginaPrincipal/vista-registrarse/vista-registrarse.component';
 import { VistaDatosPerfilMecanicoComponent } from './pages/perfilMecanico/vista-datos-perfil-mecanico/vista-datos-perfil-mecanico.component';
 
 const routes: Routes = [
   {path:'', component:VistaInicioComponent},
   {path:'inicio', component:VistaInicioSesionComponent},
-  {path:'vista-perfil-mecanico', component:VistaDatosPerfilMecanicoComponent}
+  {path:'vista-perfil-mecanico', component:VistaDatosPerfilMecanicoComponent},
+  {path:'registro', component:VistaRegistrarseComponent}
 ];
 
 @NgModule({
