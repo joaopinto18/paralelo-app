06ffa457cbb411ca37803c7ae3c0658d991b5d3f
Your NameGabriel <agbalza23@gmail.com>
2021-03-10 14:26:29 -0400

    antes de unir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaInicioSesionComponent } from './pages/paginaPrincipal/vista-inicio-sesion/vista-inicio-sesion.component';
+import { VistaInicioComponent } from './pages/paginaPrincipal/vista-inicio/vista-inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: () => import('./paginaPrincipal/vista-inicio').then(m =>m.VistaInicioComponent)},
+  {path: 'inicio', loadChildren: () => import('./paginaPrincipal/vista-inicio-sesion').then(m =>m.VistaInicioSesionComponent)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
