53c57b4326ab2b2f3bacc5147a441dc2cd1dabbd
Joao Pinto <joaopintotovar2000@gmail.com>
2021-03-10 13:47:34 -0400

    guardado resolviendo el tema del navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaInicioComponent } from './pages/paginaPrincipal/vista-inicio/vista-inicio.component';
+import { VistaDatosPerfilMecanicoComponent } from './pages/perfilMecanico/vista-datos-perfil-mecanico/vista-datos-perfil-mecanico.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'vista-inicio', component:VistaInicioComponent},
+  {path:'vista-perfil-mecanico', component:VistaDatosPerfilMecanicoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
