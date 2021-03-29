a906df8e7e23a290289b71f8df113e0aa96ab9c8
Valruja <v.rujana@correo.unimet.edu.ve>
2021-03-29 12:43:50 -0400

    vista-cliente

---
+++ src/app/pages/perfilCliente/vista-solicitar-citas/vista-solicitar-citas.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vista-solicitar-citas',
+  templateUrl: './vista-solicitar-citas.component.html',
+  styleUrls: ['./vista-solicitar-citas.component.scss']
+})
+export class VistaSolicitarCitasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clicked = false;
+  clicked2 = false;
+  clicked3 = false;
+
+}
