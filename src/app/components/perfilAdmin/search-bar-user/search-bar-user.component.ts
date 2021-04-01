a0dd64c5477776af01e9fbb28a41e6cd61c05dfc
Joao Pinto <joaopintotovar2000@gmail.com>
2021-03-31 20:10:51 -0400

    funcionalidad a cambiar rol de admin

--- src/app/components/perfilAdmin/search-bar-user/search-bar-user.component.ts
+++ src/app/components/perfilAdmin/search-bar-user/search-bar-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { take } from 'rxjs/operators';
+import { AddInfoUserServicesService } from 'src/app/services/add-info-user-services.service';
 
 @Component({
   selector: 'app-search-bar-user',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarUserComponent implements OnInit {
 
-  constructor() { }
+  public form!:FormGroup;
+
+  constructor(private userService: AddInfoUserServicesService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createInputForm1();
+    this.userService.estadoRespuesta='no se ha realizado búsqueda';
   }
 
+  createInputForm1(): void{
+    this.form=this.fb.group({
+      correo:''
+    });
+  }
+
+  async search():Promise<void>{
+    const newCar: any={
+      placa:this.form.get('correo')?.value
+    }
+    const usuario = await this.userService.BuscarUsuario(newCar.placa).valueChanges().pipe(take(1)).toPromise();
+    if(usuario[0]!=null){
+      this.userService.estadoRespuesta=usuario[0].correo;
+      console.log('se encontro el usuario');
+      alert('usuario encontrado')
+    }else{
+      this.userService.estadoRespuesta='sin resultados';
+      console.log('no se encontro el usuario');
+      alert('no se encontro el usuario')
+    }
+  }
 }
