b194fa909477b11a4aa8c53e5613bcd09b8a1cd9
Joao Pinto <joaopintotovar2000@gmail.com>
2021-04-05 16:38:28 -0400

    tartrando erro

--- src/app/pages/perfil-gerente/vista-citas-asignadas/vista-citas-asignadas.component.ts
+++ src/app/pages/perfil-gerente/vista-citas-asignadas/vista-citas-asignadas.component.ts
@@ -37,4 +37,23 @@ export class VistaCitasAsignadasComponent implements OnInit {
     })
   }
 
+  async search():Promise<void>{
+    const citaUser: any={
+      correo:this.registroVehiculoForm.get('correo')?.value
+    }
+    
+    const cita = await this.carService.BuscarVehiculo(citaUser.correo).valueChanges().pipe( take(1) ).toPromise();
+    this.vehiculoBuscado = cita;
+    if(cita[0]==undefined){
+      alert('sin resultados para esta búsqueda')
+    }else if(cita[0].estado=='orden cerrada por mecanico'){
+      alert('la orden de reparación de este vehículo ya fue cerrada');
+    }else{
+      this.correo = cita[0].correo;
+      //buscamos los datos de esta orden y llenamos los campos 
+      //this.infoVehiculo.setValue({repuestos: cita[0].repuestos, 
+      //procedimiento:cita[0].procedimiento, diagnostico:cita[0].diagnostico});
+    }
+  }
+
 }
