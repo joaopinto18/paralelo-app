8cbb22f55176aa92f9d00b3af699222d7723d58e
Joao Pinto <joaopintotovar2000@gmail.com>
2021-04-07 17:59:03 -0400

    funcionalidad btn descativar en cliente

--- src/app/pages/perfilCliente/vista-registro-vehiculo2/vista-registro-vehiculo2.component.ts
+++ src/app/pages/perfilCliente/vista-registro-vehiculo2/vista-registro-vehiculo2.component.ts
@@ -73,4 +73,14 @@ export class VistaRegistroVehiculo2Component implements OnInit {
     
     }
 
+    async desactivar(){
+      const user = await this.addCar.BuscarVehiculoIdxx(localStorage.getItem('correouser'),2).valueChanges().pipe( take(1) ).toPromise()
+      this.addCar.liberarEspacioNuevoVehiculo(user[0].placa);
+  
+      this.form.setValue({serial_motor: '',
+        modelo:'', anno:'', placa:'', fecha:''});
+  
+        alert('se ha liberado este espacio para registrar vehiculo')
+    }
+
 }
